2479f8288c395a8566b2187c6eef1e6d31de751a
kkenM <brekkentm@gmail.com>
2026-08-25 22:48:42 -0600

    Configured SQLite database with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+# Configure SQLite Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///accounts.db'
+
+# Connect SQLAlchemy to Flask
+database = SQLAlchemy(app)
 
 @app.route('/')
 def home():
